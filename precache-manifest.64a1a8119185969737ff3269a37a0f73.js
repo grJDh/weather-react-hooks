0f13e24b91a63571fb5aaf72ef67d1e7f6ed69ab
grJDh <rusmukhametov@gmail.com>
2020-05-14 18:05:27 +0300

    Updates

---
+++ precache-manifest.64a1a8119185969737ff3269a37a0f73.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f98eb552365e800862cb4723652dac78",
+    "url": "/weather-react-hooks/index.html"
+  },
+  {
+    "revision": "c3496d0e4d08926f69a5",
+    "url": "/weather-react-hooks/static/css/main.8c04e2bd.chunk.css"
+  },
+  {
+    "revision": "804ecbecb2174098e301",
+    "url": "/weather-react-hooks/static/js/2.35a919eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-react-hooks/static/js/2.35a919eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3496d0e4d08926f69a5",
+    "url": "/weather-react-hooks/static/js/main.37e013ad.chunk.js"
+  },
+  {
+    "revision": "fba69b53ba78e6d2a763",
+    "url": "/weather-react-hooks/static/js/runtime-main.479bc295.js"
+  },
+  {
+    "revision": "c97a9fc29652bb4afcdac68020e5d0f7",
+    "url": "/weather-react-hooks/static/media/Merriweather-Regular.c97a9fc2.ttf"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weather-react-hooks/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weather-react-hooks/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weather-react-hooks/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weather-react-hooks/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/weather-react-hooks/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
